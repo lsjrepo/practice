36ddb597cceacac720a7798524b14c54d48b0ed8
13083513833@163.com <15601597255ab*c>
2017-08-31 20:43:10 +0800

    ...

--- src/aliBaba/Main.java
+++ src/aliBaba/Main.java
@@ -9,65 +9,20 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<UnilateralLine> lineList = new ArrayList<UnilateralLine>();
-        while (scanner.hasNextLine()) {
-            String[] options = scanner.nextLine().split(";");
-            if (options.length < 5) {
-                break;
-            }
-            lineList.add(new UnilateralLine(options[0], options[1], options[2], options[3], options[4], options[5]));
+        Scanner sin = new Scanner(System.in);
+        int N=sin.nextInt();
+        int[] input=new int[N];
+        for(int i=0;i<input.length;i++){
+            input[i]=sin.nextInt();
         }
-        scanner.close();
-
-        // wirte your code here
-        List<String> list=new ArrayList<>();
-        for (int i=0;i<lineList.size();i++){
-            for(int j=0;j<lineList.size();j++){
-                if (lineList.get(i).getECen().equals(lineList.get(j).getSCen())){
-                    if(lineList.get(i).getEPro().equals(lineList.get(j).getSPro())){
-                        if (lineList.get(i).getTType().equals(lineList.get(j).getTType())){
-                            list.add("rule1:"+lineList.get(i).getId()+"+"+lineList.get(j).getId());
-                        }
-                    }
-
-                }
-
-            }
-
-
-        }
-        List<String> result = calculateUnilateral(lineList);
-
-        for (String str : result) {
-            System.out.println(str);
-        }
+        int K=sin.nextInt();
+        System.out.println(maxSubString(input,K);
     }
-    public static List<String> calculateUnilateral(List<UnilateralLine> lineList) {
-        List<String> result = new ArrayList<String>();
-        return result;
+
+    public static boolean maxSubString(int[] input, int k) {
+        List<Integer> list=new ArrayList<>();
+        int maxLength=0;
+
     }
-    public static class UnilateralLine {
-        private String id;
-        private String sCen;//出发分拨
-        private String sPro;//出发省
-        private String eCen;//到达分拨
-        private String ePro;//到达省
-        //9.6m/17.5m
-        private String tType;//车型
-        public UnilateralLine(String id, String sCen, String sPro, String eCen, String ePro,String tType) {
-            this.id = id;this.sCen = sCen;this.sPro = sPro;this.eCen = eCen;this.ePro = ePro;this.tType = tType;}
-        public String getId() {return id;}
-        public void setId(String id) {this.id = id;}
-        public String getSCen() {return sCen;}
-        public void setSCen(String ePro) {this.ePro = ePro;}
-        public String getSPro() {return sPro;}
-        public void setSPro(String sPro) {this.sPro = sPro;}
-        public String getECen() {return eCen;}
-        public void setECen(String eCen) {this.eCen = eCen;}
-        public String getEPro() {return ePro;}
-        public void setEPro(String ePro) {this.ePro = ePro;}
-        public String getTType() {return tType;}
-        public void setTType(String tType) {this.tType = tType;}
-    }
+
 }
