30c16de6ec5a6e304496402e72e855ded0760bd9
saijie.lu <saijie.lu@dingxianginc.com>
2018-09-18 23:57:58 +0800

    BST TREE REVIEW

---
+++ src/basic/Tree/TIRE/Test01.java
@@ -0,0 +1,16 @@
+package basic.Tree.TIRE;
+
+/**
+ * @author saijie.lu
+ * @create 2018-09-18 23:38
+ * 线段树的使用场景:
+ * 给定一个区间
+ * 更新:更新区间中一个元素或者一个区间的值
+ * 查询区间[i,j]的最大值或者最小值，或者区间数字和，区间本省是固定的
+ * 线段树存储，通过数字来存储需要4n的空间
+ **/
+public class Test01 {
+  public static void main(String[] args) {
+    //
+  }
+}
